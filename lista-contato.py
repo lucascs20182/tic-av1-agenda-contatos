8ce34f4761775e9450430ef5de05fbc5969883e4
Lucas Cruz <lucas.dsn.imb@alterdata.com.br>
2022-04-02 21:43:16 -0300

    ajuste na validação de celular/telefone

--- lista-contato.py
+++ lista-contato.py
@@ -1,17 +1,41 @@
-def novoContato(nome, ddd , telefone):
+def novo_contato(nome, ddd , telefone):
     global id_novo_contato
 
     contato = {'id': id_novo_contato + 1, 'nome': nome, 'DDD': ddd, 'telefone': telefone}
     id_novo_contato += 1
     return contato
 
+
+
 def obter_celular_ou_telefone():
-    numero_do_contato = input('Número do contato:')
-    while len(numero_do_contato) != 8 or 9:
+    numero_do_contato = str(input('Número do contato: '))
+
+    while not is_numero_do_contato_valido(numero_do_contato):
         print(f'Número inválido, tente novamente!')
-        numero_do_contato = input('Número do contato: ')
+        numero_do_contato = str(input('Número do contato: '))
+
     return numero_do_contato
-    
+
+
+
+def is_numero_do_contato_valido(numero_do_contato):
+    return len(numero_do_contato) != 8 or len(numero_do_contato) != 9
+
+
+
+def formatar_numero_do_contato(numero_do_contato):
+    numero_do_contato_formatado = ''
+
+    if len(numero_do_contato) == 8:
+        numero_do_contato_formatado = f'{numero_do_contato[0:4]}-{numero_do_contato[4:8]}'
+
+    if len(numero_do_contato) == 9:
+        numero_do_contato_formatado = f'{numero_do_contato[0:5]}-{numero_do_contato[5:9]}'
+
+    return numero_do_contato_formatado
+
+
+
 # start point do programa
 if __name__ == '__main__':
     global id_novo_contato
@@ -27,15 +51,15 @@ if __name__ == '__main__':
         print('3 - Mostrar contatos')
         print('4 - Sair\n')
         opcao = int(input('Opção: '))
-        
+
         if(not (opcao >= 1 and opcao <= 4)):
             print('Opção inválida! Tente novamente.')
         elif(opcao == 1):
             nome = str(input('Nome do contato: '))
-            ddd = int(input('DDD: '))
+            ddd = str(input('DDD: '))
             numero_do_contato = obter_celular_ou_telefone()
 
-            agenda_de_contatos.append(novoContato(nome, ddd, numero_do_contato))
+            agenda_de_contatos.append(novo_contato(nome, ddd, numero_do_contato))
             print(f'contato {nome} salvo!')
         elif(opcao == 2):
             print('Código de remover contato aqui')
